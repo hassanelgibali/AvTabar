6339343ef263684cb6f29d990aa1ce7232e22086
V21HElGebally <hassan.gamalelgebally@vodafone.com>
2022-07-20 12:41:32 +0200

    Initial commit

---
+++ Example/AVTabBar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AVTabBar
+//
+//  Created by V21HElGebally on 07/20/2022.
+//  Copyright (c) 2022 V21HElGebally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
